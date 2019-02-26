065727de49ae88f821d76c96c26b6901537ff950
lucas von lachmann <lucasvonlachmann@hotmail.com>
2019-02-26 17:37:59 -0300

    finished without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |t|
+  article = Article.new(title: Faker::Name.unique.name, content: Faker::Name.unique.name)
+  article.save
+end
